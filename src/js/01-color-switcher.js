1c1636e4ac062f919e76137ffb4cd36108626176
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-22 20:17:59 +0300

    color-switcher-update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    backgroundColor: document.querySelector('body'),
+}
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+
+function onStartBtnClick() {
+    refs.startBtn.disabled = true;
+    timerId = setInterval(changeBackgroundColor, 1000);
+
+}
+
+function changeBackgroundColor() {
+    const randomColor = getRandomHexColor();
+    refs.backgroundColor.style.backgroundColor = randomColor;
+}
+
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+function onStopBtnClick() {
+    refs.startBtn.disabled = false;
+    clearInterval(timerId)
+}
