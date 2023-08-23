301cdfbee57c0435dd3cb20a4a3b36fdfa6c4370
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-23 21:28:28 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,8 @@ const refs = {
     backgroundColor: document.querySelector('body'),
 }
 
+let timerId = null;
+
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
